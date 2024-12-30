7b1123888525363e58f95be86a9b51ce5dc8d6da
Klaudio Merja <merjakla03@gmail.com>
2024-12-30 20:51:49 +0100

    Prima implementazione di Gulpease

--- gulpease.js
+++ gulpease.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const path = require("path");
+const textStats = require("text-stats");
 
 /**
  *
@@ -33,7 +34,7 @@ function searchFile(dir) {
  * @param {string} word  la parola da cercare all'interno del file
  * @returns `true` se la prima occorrenza è stata individuata ed è riferita al glossario, false altrimenti (anche il caso in cui il riferimento al glossario non sia stato inserito alla prima occorrenza)
  */
-function checkOccurrence(filePath, word) {
+function calcGulpease(filePath) {
   const blob = fs.readFileSync(filePath);
   const commentPattern = /\/\*[\s\S]*?\*\/|([^\\:]|^)\/\/.*$/gm; //https://stackoverflow.com/questions/5989315/regex-for-match-replacing-javascript-comments-both-multiline-and-inline
   const importPattern = /#import ".*"\s*:\s*\*/g;
@@ -53,22 +54,12 @@ function checkOccurrence(filePath, word) {
     .replaceAll(backlogFuncPattern, "")
     .toLowerCase();
 
-  const glossaryRefPattern = new RegExp(
-    `\\b${word.toLowerCase()}\\b|\\*${word.toLowerCase()}\\*|_${word.toLowerCase()}_|#rifglossario\\("${word.toLowerCase()}"\\)`
-  );
-  const match = content.match(glossaryRefPattern);
-  return match ? match[0].length == word.length + 17 : true;
+  const stats = textStats.stats(content);
+  console.log(`${filePath}: ${stats.gulpease}`);
 }
 
 const glossario = require("./glossario.json");
 
 searchFile(".").forEach((path) => {
-  for (letter in glossario) {
-    for (word in glossario[letter]) {
-      if (checkOccurrence(path, word) == false)
-        console.log(
-          `\x1b[1;35m${word}\x1b[0;35m in \x1b[4;35m${path}\x1b[0;35m mancante \x1b[0m`
-        );
-    }
-  }
+  calcGulpease(path);
 });
